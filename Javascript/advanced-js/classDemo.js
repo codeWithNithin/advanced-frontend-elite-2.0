e9dc8d1e364d243d1656d63eb0d9912555b1f58c
nithin kumar <nithinkottary25@gmail.com>
2024-07-26 12:10:11 +0530

    live class code

--- Javascript/advanced-js/classDemo.js
+++ Javascript/advanced-js/classDemo.js
@@ -1,23 +1,48 @@
 // class -> blue print of the real world entity
 class Product {
   // these are all properties, technically, its called data members
-  name;
-  price;
-  category;
-  description;
-  rating;
+  // To make a property private, we use # before the property
+  #name;
+  #price;
+  #category;
+  #description;
+  #rating;
 
   //very special member function of every class
   constructor(name, price, category, description, rating) {
     // here this referes to the empty object that is created when new keyword is called.
-    this.name = name;
-    this.price = price;
-    this.category = category;
-    this.description = description;
-    this.rating = rating;
+    this.#name = name;
+    this.#price = price;
+    this.#category = category;
+    this.#description = description;
+    this.#rating = rating;
     console.log('constructor called');
     // if constructor returns non-primitive value, then that manually returned object will be assigned to called variable
     //if no return statement or if primitive values are returned, then value of this keyword will be assigned to called variable
+  }
+
+  set setProductName(name) {
+    this.#name = name;
+  }
+
+  get getProductName() {
+    return this.#name;
+  }
+
+  set setProductPrice(price) {
+    this.#price = price;
+  }
+
+  get getProductPrice() {
+    return this.#price;
+  }
+
+  set setProductCategory(price) {
+    this.#price = price;
+  }
+
+  get getProductCategory() {
+    return this.#category;
   }
 
   // These are all methods, technically, its called member functions
@@ -27,6 +52,7 @@ class Product {
 
   displayProduct() {
     console.log('display products here...');
+    console.log(this.#name, this.#price, this.#category, this.#description);
   }
 
   buyProduct() {
@@ -34,13 +60,15 @@ class Product {
   }
 }
 
-// const p1 = new Product(
-//   'laptop',
-//   1000,
-//   'electronics',
-//   'laptop with 8gb ram',
-//   4.5
-// );
+const p1 = new Product(
+  'laptop',
+  1000,
+  'electronics',
+  'laptop with 8gb ram',
+  4.5
+);
+
+p1.displayProduct();
 
 // Related to this keyword...
 // In most of the cases, this refers to call site
